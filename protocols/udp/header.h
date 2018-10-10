64275361df4a62c1b321c752e7e67b4431990e99
Jason <williamscommajason@github.com>
2018-10-09 19:44:15 -0700

    updated header file

--- protocols/udp/header.h
+++ protocols/udp/header.h
@@ -1,3 +1,19 @@
+#ifndef UDP_H
+#define UDP_H
+
+#include <stdint.h>
+#include <arpa/inet.h>
+
+#define UDP_HDR_SIZE 8
+#define INET_HDR_LEN 5
+
+unsigned int build_udp(struct sockaddr_in src_addr, struct sockaddr_in dst_addr, uint8_t *udp_packet, uint8_t *data, unsigned int data_size);
+
+unsigned int build_ip(struct in_addr src_addr, struct in_addr dst_addr, uint8_t protocol, uint8_t *ip_packet, uint8_t *data, unsigned int data_size);
+
+void send_udp(int raw_sock, struct sockaddr_in src_addr, struct sockaddr_in dst_addr, uint8_t *data, unsigned int data_size);
+
+
 struct ipheader
 {
 	unsigned char		iph_ihl:5, iph_ver:4;
@@ -9,8 +25,8 @@ struct ipheader
 	unsigned char		iph_ttl;
 	unsigned char		iph_protocol;
 	unsigned short int	iph_chksum;
-	unsigned int 		iph_sourceip;
-	unsigned int		iph_destip;
+	uint32_t		 	iph_sourceip;
+	uint32_t			iph_destip;
 }; /* total ipheader length: 21 bytes ( = 168 bits) */
 
 
@@ -20,7 +36,7 @@ struct udpheader
 	unsigned short int udph_srcport;
 	unsigned short int udph_destport;
 	unsigned short int udph_len;
-	unsigned short int udph_chksum;
+	//unsigned short int udph_chksum;
 }; /* total udp header length: 8 bytes (= 64 bits) */
 
-
+#endif
